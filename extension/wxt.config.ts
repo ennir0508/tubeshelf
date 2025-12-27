68f95eb1b8f04a821b59a80376b9d3f2bdb9f098
ebikatsu <ennir0508@gmail.com>
2025-12-27 23:24:39 +0900

    fix: Tailwind CSS を追加

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -20,7 +21,12 @@ export default defineConfig({
       hmr: {
         port: 3000,
       }
-    }
+    },
+    plugins: [tailwindcss()],
+    alias: {
+      '@': './',
+    },
+
   }),
   manifest: {
     action: {
